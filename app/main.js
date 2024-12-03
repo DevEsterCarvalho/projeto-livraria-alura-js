899421be673630fd01afb5973d0dff7bb7aa0ea9
DevEsterCarvalho <estercarvalho205@gmail.com>
2024-12-03 20:32:31 -0300

    first commit

---
+++ app/main.js
@@ -0,0 +1,11 @@
+let livros = []
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaApi()
+
+async function getBuscarLivrosDaApi(){
+    console.log("função chamada")
+    const res = await fetch(endpointDaAPI)
+    livros = await res.json()
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
+}
